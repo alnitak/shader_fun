8f1fdaf7f54d2ae327fa6125878c40cc4e7c1534
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2026-09-07 12:18:34 +0200

    playback test moved to example tests

--- example/test/playback_test.dart
+++ example/test/playback_test.dart
@@ -1,5 +1,4 @@
-// ignore: avoid_relative_lib_imports
-import '../example/lib/shadertoy_studio.dart';
+import 'package:example/shadertoy_studio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -28,41 +27,25 @@ void main() {
     // 1. Tap pause in viewport
     await tester.tap(find.byIcon(Icons.pause));
     await tester.pump();
-
-    // Now paused: viewport shows play_arrow, header shows play_circle_fill, badge shows PAUSED
-    expect(find.byIcon(Icons.play_arrow), findsOneWidget);
-    expect(find.byIcon(Icons.play_circle_fill), findsOneWidget);
     expect(find.text('PAUSED'), findsOneWidget);
-
-    // 2. Tap play in editor header
-    await tester.tap(find.byIcon(Icons.play_circle_fill));
-    await tester.pump();
-
-    // Now playing again
-    expect(find.byIcon(Icons.pause), findsOneWidget);
-    expect(find.byIcon(Icons.pause_circle_filled), findsOneWidget);
-    expect(find.text('PAUSED'), findsNothing);
-
-    // 3. Tap pause in editor header
-    await tester.tap(find.byIcon(Icons.pause_circle_filled));
-    await tester.pump();
-
     expect(find.byIcon(Icons.play_arrow), findsOneWidget);
-    expect(find.byIcon(Icons.play_circle_fill), findsOneWidget);
-    expect(find.text('PAUSED'), findsOneWidget);
 
-    // 4. Tap play in viewport
+    // 2. Tap play in viewport
     await tester.tap(find.byIcon(Icons.play_arrow));
     await tester.pump();
-
+    expect(find.text('PAUSED'), findsNothing);
     expect(find.byIcon(Icons.pause), findsOneWidget);
-    expect(find.byIcon(Icons.pause_circle_filled), findsOneWidget);
 
-    // 5. Verify compile button is present
-    expect(find.byIcon(Icons.bolt), findsOneWidget);
-
-    // Pause before unmounting
-    await tester.tap(find.byIcon(Icons.pause));
+    // 3. Tap pause in editor bottom bar
+    await tester.tap(find.byIcon(Icons.pause_circle_filled));
     await tester.pump();
+    expect(find.text('PAUSED'), findsOneWidget);
+    expect(find.byIcon(Icons.play_circle_filled), findsOneWidget);
+
+    // 4. Tap play in editor bottom bar
+    await tester.tap(find.byIcon(Icons.play_circle_filled));
+    await tester.pump();
+    expect(find.text('PAUSED'), findsNothing);
+    expect(find.byIcon(Icons.pause_circle_filled), findsOneWidget);
   });
 }
